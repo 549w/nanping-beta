77d42472653353528552a6097cc96839eb700bd3
549w <3171953764@qq.com>
2026-07-02 21:44:36 +0800

    feat: 添加中文字体支持并优化页面结构，优化页面样式和布局细节

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -58,9 +58,9 @@ export function renderStars(rating) {
   let html = '<span class="star-rating" title="' + r + ' 分">';
   for (let i = 1; i <= 5; i++) {
     if (i <= r) {
-      html += '<span class="material-symbols-outlined">star</span>';
+      html += '<span class="material-symbols-outlined star-filled">star</span>';
     } else {
-      html += '<span class="material-symbols-outlined star-empty">star_outline</span>';
+      html += '<span class="material-symbols-outlined star-empty">star</span>';
     }
   }
   html += "</span>";
